cff8e5b17ddfb6459b04f1b42997ada2178414e6
SerbanCrasovan <serban.crasovan@student.upt.ro>
2022-11-11 14:34:21 +0200

    8th ex

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  for(let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let mul = 1;
+  for(let i = 0; i < array.length; i++) {
+    mul *= array[i];
+  }
+  return mul;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let mul = 1;
+  for(let i = 1; i <= a; i++) {
+    mul *= i;
+  }
+  return mul;
 };
 
 // Do not edit below this line
