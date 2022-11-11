945b98b2e7f777166755f15e6d95e01b0d5ae1fd
SerbanCrasovan <serban.crasovan@student.upt.ro>
2022-11-11 15:04:41 +0200

    10th ex

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if(number < 0) {
+        return "OOPS";
+    }
+    first = 1;
+    second = 1;
+    if(number == 1 || number == 2) {
+        return 1;
+    }
+    else {
+    for(let i = 2; i < number; i++) {
+        aux = second;
+        second = first + second;
+        first = aux;
+        
+    } 
+    return second;
+    }
+    
 };
 
 // Do not edit below this line
