41ab9ea472866d0fb3bbe1f587fbec8ca456cc34
SerbanCrasovan <serban.crasovan@student.upt.ro>
2022-11-11 14:56:07 +0200

    9th ex

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    processedString = word.toLowerCase().replace(/[^a-z]/g, "");   
+    if(processedString.split("").reverse().join("") == processedString) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
