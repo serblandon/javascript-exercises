79c1536c2febfd2fb72042732773493679148b69
SerbanCrasovan <serban.crasovan@student.upt.ro>
2022-11-11 14:28:35 +0200

    7th ex

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+    temp = (temp - 32) * 5 / 9 ;
+    temp = Number(temp.toFixed(1));
+    return temp;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+    temp = temp * 9 / 5 + 32;
+    temp = Number(temp.toFixed(1));
+    return temp;
 };
 
 // Do not edit below this line
