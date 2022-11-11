012013d75e3c2eed0edd89c251dbecf492dc56ce
SerbanCrasovan <serban.crasovan@student.upt.ro>
2022-11-11 14:15:52 +0200

    5th ex

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(left, right) {
+    let sum = 0;
+    if(left < 0 || right < 0) {
+        return "ERROR";
+    }
+    if(!Number.isInteger(left) || (!Number.isInteger(right))) {
+        return "ERROR";
+    }
+    if(left > right) {
+        const tmp = left;
+        left = right;
+        right = tmp;
+    }
+    for(let i = left; i <= right; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
