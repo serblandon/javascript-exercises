443f42f459f8ddb4128ed93d46f9aee91baca7ec
SerbanCrasovan <serban.crasovan@student.upt.ro>
2022-11-11 13:38:46 +0200

    3rd ex

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const arr = string.split("");
+    const rev_arr = arr.reverse();
+    const result = rev_arr.join("");
+    return result;
 };
 
 // Do not edit below this line
